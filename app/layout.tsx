1e12795ce3644440889d13d5394421d190d2471d
Radu Bordea <radu.bordea.dev@gmai.com>
2025-11-20 00:54:12 +0100

    change to dark mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
       <body>
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
+          defaultTheme="dark"
           enableSystem
           disableTransitionOnChange
         >
