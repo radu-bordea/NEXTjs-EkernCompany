990ec858ac83f87d5b5e9053667aef0991a3ced6
Radu Bordea <radu.bordea.dev@gmai.com>
2025-11-20 00:33:58 +0100

    fix components typo name

--- Components/Navbar.tsx
+++ Components/Navbar.tsx
@@ -3,7 +3,7 @@
 import { useState } from "react";
 import Link from "next/link";
 import { Menu, X } from "lucide-react";
-import { ThemeToggle } from "@/components/theme-toggle";
+import { ThemeToggle } from "@/Components/theme-toggle";
 
 const navLinks = [
   { href: "#home", label: "Home" },
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 // app/layout.tsx
 import Navbar from "@/Components/Navbar";
 import "./globals.css";
-import { ThemeProvider } from "@/components/theme-provider";
+import { ThemeProvider } from "@/Components/theme-provider";
 import Footer from "@/Components/Footer";
 import SocialSidebar from "@/Components/SocialSidebar";
 
